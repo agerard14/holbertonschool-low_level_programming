ed505b6c1b11ce6641043ad6538c12832e05c348
gerard <gerardqp14@gmail.com>
2022-06-07 18:48:17 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all avaliable numbers in base 10
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar(10);
+	return (0);
+}
