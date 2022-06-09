edb5b083d5925d4efda337b47fe9ebe937da9a95
gerard <gerardqp14@gmail.com>
2022-06-09 12:38:21 -0700

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -13,7 +13,7 @@
 
 int _isalpha(int c)
 {
-	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ',')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ',')
 		return (1);
 	else
 		return (0);
