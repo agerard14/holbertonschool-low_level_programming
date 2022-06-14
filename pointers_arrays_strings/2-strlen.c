22fa3c5432cc4fe43fee2febb545fdf26eb05644
gerard <gerardqp14@gmail.com>
2022-06-14 13:07:13 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiply 2 numbers.
+ *
+ * @a: number
+ *
+ * @b: number
+ *
+ * Return: The product of the multiply of 2 numbers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
