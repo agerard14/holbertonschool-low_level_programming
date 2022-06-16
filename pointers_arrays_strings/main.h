a4edb3262e9efec80d80dfb678a8a9e132162a4f
gerard <gerardqp14@gmail.com>
2022-06-16 10:06:26 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,3 +17,25 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _putchar(char c);
+
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *C);
+
+char *cap_string(char *c);
+
+char *leet(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
